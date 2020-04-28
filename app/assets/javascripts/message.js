af049fd181aeb83e1a52dfa87dbd818f9916882e
sirokumasan <fsho2262@gmail.com>
2020-04-28 15:58:08 +0900

    非同期通信を実装修正(1)

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -33,7 +33,6 @@ $(function() {
 
   $('#new_message').on('submit', function(e) {
     e.preventDefault()
-    console.log("hoge");
     var formData = new FormData(this);
     var url = $(this).attr('action');
     $.ajax({
